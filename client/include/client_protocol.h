5238e5a504fa84a972c47bfe0f59ccd30013bda4
tyreller <thiagoyair1@gmail.com>
2026-05-17 04:11:27 -0300

    feat: refactor protocol and add command pattern - Add Command hierarchy for server-side client commands - Move protocol encoding/decoding logic to separate encoder/decoder classes - Keep DTOs for transport-layer messages

--- client/include/client_protocol.h
+++ client/include/client_protocol.h
@@ -2,34 +2,31 @@
 #define CLIENT_PROTOCOL_H
 
 #include "../../common/include/binary_protocol.h"
-#include "../../common/include/dto/client_command_dto.h"
+#include "../../common/include/dto/command_dto.h"
 #include "../../common/include/dto/server_event_dto.h"
-#include "../../common/include/protocol_codes.h"
 #include "../../common/include/socket.h"
-#include <string>
 
 /*
   ClientProtocol
-  Maneja la comunicación de su cliente con el servidor.
+  Maneja la comunicación de el cliente con el servidor.
+
   Implementa BinaryProtocol para enviar y recibir mensajes usando el protocolo
   definido.
-
-  Ejemplo de uso: client.protocol.send_register_player("Alice");
 */
 
+class ClientDecoder;
+class ClientEncoder;
+
 class ClientProtocol : public BinaryProtocol {
+  // Para que pueda acceder a los metodos privados/protegidos de ClientProtocol
+  friend class ClientDecoder;
+  friend class ClientEncoder;
+
 public:
   explicit ClientProtocol(Socket &socket);
 
   ServerEvent recv_event();
-  void send_command(ClientCommand command);
-
-private:
-  void send_register_player(const RegisterPlayerCommand &player_name);
-  void send_resurrect();
-  void send_meditate();
-  void send_private_message(const PrivateMessageCommand &private_message);
-  void send_exit();
+  void send_command(const CommandDTO &command);
 };
 
 #endif // CLIENT_PROTOCOL_H
--- server/src/server_protocol.cpp
+++ server/src/server_protocol.cpp
@@ -1,67 +1,17 @@
 #include "../include/server_protocol.h"
-#include "../../common/include/constants.h"
-#include "../../common/include/exceptions/protocol_error.h"
-#include <arpa/inet.h>
+
+#include "../../common/include/command/command.h"
+#include "../include/server_decoder.h"
+#include "../include/server_encoder.h"
 
 ServerProtocol::ServerProtocol(Socket &socket) : BinaryProtocol(socket) {}
 
-ClientCommand ServerProtocol::recv_command() {
-  auto opcode = static_cast<protocol::ClientOpcode>(recv_uint8());
-
-  switch (opcode) {
-  case protocol::ClientOpcode::REGISTER_PLAYER:
-    return RegisterPlayerCommand{recv_string()};
-
-  case protocol::ClientOpcode::RESURRECT:
-    return ResurrectCommand{};
-
-  case protocol::ClientOpcode::MEDITATE:
-    return MeditateCommand{};
-
-  case protocol::ClientOpcode::PRIVATE_MESSAGE: {
-    std::string target = recv_string();
-    std::string msg = recv_string();
-    return PrivateMessageCommand{std::move(target), std::move(msg)};
-  }
-
-  case protocol::ClientOpcode::EXIT:
-    return ExitCommand{};
-
-  default:
-    throw ProtocolError(
-        "Unable to process Command: Unknown client opcode received");
-  }
+// Devuelve el comando recibido ya parseado
+std::unique_ptr<Command> ServerProtocol::recv_command(PlayerId caller) {
+  return ServerDecoder::decode(*this, caller);
 }
 
+// Envia el evento dado al cliente usando el protocolo definido
 void ServerProtocol::send_event(const ServerEvent &event) {
-  if (std::holds_alternative<ChatMessageEvent>(event)) {
-    send_chat_message(std::get<ChatMessageEvent>(event));
-
-  } else if (std::holds_alternative<NpcDefeatedEvent>(event)) {
-    send_npc_defeated(std::get<NpcDefeatedEvent>(event));
-
-  } else if (std::holds_alternative<PlayerMovedEvent>(event)) {
-    send_player_moved(std::get<PlayerMovedEvent>(event));
-
-  } else {
-    throw ProtocolError("Unable to send event: Unknown server event");
-  }
-}
-
-void ServerProtocol::send_chat_message(const ChatMessageEvent &chat_msg) {
-  send_uint8(static_cast<uint8_t>(protocol::ServerOpcode::CHAT_MESSAGE));
-  send_string(chat_msg.sender);
-  send_string(chat_msg.message);
-}
-
-void ServerProtocol::send_npc_defeated(const NpcDefeatedEvent &npc_defeated) {
-  send_uint8(static_cast<uint8_t>(protocol::ServerOpcode::NPC_DEFEATED));
-  send_uint8(npc_defeated.npc_id);
-}
-
-void ServerProtocol::send_player_moved(const PlayerMovedEvent &player_moved) {
-  send_uint8(static_cast<uint8_t>(protocol::ServerOpcode::PLAYER_MOVED));
-  send_uint32(player_moved.player_id);
-  send_uint16(player_moved.x);
-  send_uint16(player_moved.y);
+  ServerEncoder::encode(*this, event);
 }
