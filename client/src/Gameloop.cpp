dcceaca881f8cff82b90e10a6ce85f74456162d7
Federico Tomas <fparsosn@fi.uba.ar>
2026-05-17 16:00:57 -0300

    movimiento

--- client/src/Gameloop.cpp
+++ client/src/Gameloop.cpp
@@ -1,97 +1,99 @@
 #include "Gameloop.h"
 
+Gameloop::Gameloop(Queue<Command> &receptionQueue, Queue<Command> &sendingQueue,
+                   ShutdownEvent &shutdownEvent)
+    : receptionQueue(receptionQueue), sendingQueue(sendingQueue),
+      shutdownEvent(shutdownEvent) {
 
-Gameloop::Gameloop(Queue<Command> &receptionQueue, 
-                   Queue<Command> &sendingQueue, 
-                   ShutdownEvent &shutdownEvent) :
-                   receptionQueue(receptionQueue),
-                   sendingQueue(sendingQueue),
-                   shutdownEvent(shutdownEvent)
-{
-
-    initSDL();
+  initSDL();
 }
 
+void Gameloop::run() {
+  initResources();
+  unsigned int it = 0;
 
-void Gameloop::run() 
-{
-    initResources();    
-    unsigned int it = 0;
+  ConstantRateLoop rateloop(FPS);
 
-    ConstantRateLoop rateloop(FPS);
+  while (!shutdownEvent.finished()) {
 
-    while (!shutdownEvent.finished()) {
+    try {
 
-        try {
-            
-            handleEvents();
-            updateStateFromServer();
-            clearDisplay();
-            updateAnimationFrames(it);
-            render();
+      handleEvents();
+      updateStateFromServer();
+      clearDisplay();
+      updateAnimationFrames(it);
+      render();
 
-        } catch (const ClosedQueue& e) {
+    } catch (const ClosedQueue &e) {
 
-            return;
+      return;
 
-        } catch (const WindowClosed& e) {
-            // TODO: envio comando de cierre al servidor
-            shutdownEvent.put(ShutdownReason::SDLQuit);
-            return;
-        } catch (...) {
-            
-            return;
-        }
-        
+    } catch (const WindowClosed &e) {
+      // TODO: envio comando de cierre al servidor
+      shutdownEvent.put(ShutdownReason::SDLQuit);
+      return;
+    } catch (...) {
 
-        rateloop.updateTimer(it);
+      return;
     }
+
+    rateloop.updateTimer(it);
+  }
 }
 
+void Gameloop::initSDL() {
+  this->window = std::make_unique<SDL2pp::Window>(
+      SDL2pp::Window("Argentum Online", SDL_WINDOWPOS_UNDEFINED,
+                     SDL_WINDOWPOS_UNDEFINED, 720, 410, SDL_WINDOW_MINIMIZED));
 
-
-void Gameloop::initSDL()
-{
-    this->window = std::make_unique<SDL2pp::Window>(
-        SDL2pp::Window("Argentum Online",
-			SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-			720, 410,
-			SDL_WINDOW_MINIMIZED)
-    );
-    
-    this->renderer = std::make_unique<SDL2pp::Renderer>(
-        SDL2pp::Renderer(*window, -1, SDL_RENDERER_ACCELERATED)
-    );
-
+  this->renderer = std::make_unique<SDL2pp::Renderer>(
+      SDL2pp::Renderer(*window, -1, SDL_RENDERER_ACCELERATED));
 }
 
-void Gameloop::updateStateFromServer()
-{
+void Gameloop::updateStateFromServer() {}
+
+void Gameloop::clearDisplay() {
+  if (renderer) {
+    renderer->Clear();
+  }
 }
 
-void Gameloop::clearDisplay()
-{
-}
+void Gameloop::handleEvents() {
+  SDL_Event event;
+  while (SDL_PollEvent(&event)) {
+    if (event.type == SDL_QUIT) {
+      throw WindowClosed("Window was closed by the user");
 
-void Gameloop::handleEvents()
-{
-    SDL_Event event;
-    while (SDL_PollEvent(&event)) {
-        if (event.type == SDL_QUIT) 
-            throw WindowClosed("Window was closed by the user");
+    } else if (event.type == SDL_KEYDOWN) {
+      switch (event.key.keysym.sym) {
+      case SDLK_LEFT:
+      case SDLK_a:
+        sendingQueue.push(Command(CommandType::MoveLeft));
+        break;
+      case SDLK_RIGHT:
+      case SDLK_d:
+        sendingQueue.push(Command(CommandType::MoveRight));
+        break;
+      case SDLK_UP:
+      case SDLK_w:
+        sendingQueue.push(Command(CommandType::MoveUp));
+        break;
+      case SDLK_DOWN:
+      case SDLK_s:
+        sendingQueue.push(Command(CommandType::MoveDown));
+        break;
+      default:
+        break;
+      }
+
+    } else if (event.type == SDL_KEYUP) {
+      // key release, no lo usamos aun no se si lo vamos a usar
     }
+  }
 }
 
+void Gameloop::updateAnimationFrames(unsigned int it) {}
 
+void Gameloop::render() {}
 
-void Gameloop::updateAnimationFrames(unsigned int it)
-{
-}
-
-void Gameloop::render()
-{
-}
-
-void Gameloop::initResources()
-{
-}
+void Gameloop::initResources() {}
