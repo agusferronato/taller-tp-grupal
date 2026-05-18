fd357b7111454c53f67004f02b12c09ce9fdf4b0
agusferronato <aferronato@fi.uba.ar>
2026-05-18 18:25:53 -0300

    add assets, player and camera

--- client/src/Gameloop.cpp
+++ client/src/Gameloop.cpp
@@ -55,19 +55,22 @@ void Gameloop::initSDL() {
 }
 
 void Gameloop::updateStateFromServer() {
+
   std::unique_ptr<CommandDTO> cmd;
+
   while (receptionQueue.try_pop(cmd)) {
+
     switch (cmd->getCode()) {
+
         case protocol_codes::PLAYER_MOVED
-        // actualizar estado del jugadores
-        break;
+
+          break;
         default:
-        //TODO el resto de comandos
-        break;
+
+          break;
     }
-    // TODO: actualizar estado mapeando entidades (juagdores, fondo, 
-    // etc) a texturas en memoria
   }
+
 }
 
 void Gameloop::clearDisplay() {
@@ -77,8 +80,10 @@ void Gameloop::clearDisplay() {
 }
 
 void Gameloop::handleEvents() {
+
   SDL2pp::Event event;
   while (event.Poll()) {
+
     switch (event.GetType()) {
     case SDL_QUIT:
       throw WindowClosed("Window was closed by the user");
@@ -111,24 +116,6 @@ void Gameloop::handleEvents() {
 
     case SDL_KEYUP:
       switch (event.key.keysym.sym) {
-        /*
-      case SDLK_LEFT:
-      case SDLK_a:
-        // TODO: Implementar StopMove con nuevo DTO
-        break;
-      case SDLK_RIGHT:
-      case SDLK_d:
-        // TODO: Implementar StopMove con nuevo DTO
-        break;
-      case SDLK_UP:
-      case SDLK_w:
-        // TODO: Implementar StopMove con nuevo DTO
-        break;
-      case SDLK_DOWN:
-      case SDLK_s:
-        // TODO: Implementar StopMove con nuevo DTO
-        break;
-        */
       default:
         break;
       }
@@ -140,20 +127,16 @@ void Gameloop::handleEvents() {
   }
 }
 
+
 void Gameloop::updateAnimationFrames(unsigned int it) {
-  // TODO: avanzar en memoria si es necesario
 }
+
+
 
 void Gameloop::render() {
-  // TODO: renderizar el estado actual del juego
-  renderer->Copy(resources.background, nullptr, nullptr);
-  renderer->Copy(resources.player, nullptr, nullptr);
 }
 
+
+
 void Gameloop::initResources() {
-  // TODO: cargar todo en memoria
-  
-  // por la demo se tien precaragado en ResourcesTesting.hh
-  resources.player = Player(SDL2pp::Texture(renderer, ResourcesTesting::player) , NULL);
-  resources.background = SDL2pp::Texture(renderer, ResourcesTesting::background);
 }
