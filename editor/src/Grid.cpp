9b8345ab79bd5cd021279b3ddff9ff2f60cadf6a
agusferronato <aferronato@fi.uba.ar>
2026-05-25 15:53:46 -0300

    logica prioridades de renderizado

--- editor/src/Grid.cpp
+++ editor/src/Grid.cpp
@@ -14,7 +14,12 @@ Grid::Grid(Camera& camera) : camera(camera) {
 void Grid::setGridTexture(TextureMap& textureMap, int texture_id)
 {
 
-    SDL2pp::Texture& txt = textureMap.getTexture(texture_id);
+    if (thereAreAssignedTextures(textureMap, texture_id))
+        return;
+
+    TextureInMap& txtInMap = textureMap.getTexture(texture_id);
+    SDL2pp::Texture& txt = txtInMap.txt;
+
 
     int rows = std::ceil((float)txt.GetHeight() / GRID_SIZE_PX);
     int columns = std::ceil((float)txt.GetWidth() / GRID_SIZE_PX);
@@ -27,7 +32,8 @@ void Grid::setGridTexture(TextureMap& textureMap, int texture_id)
 
         for (int j = item_hover_j; j < item_hover_j + columns; j++) {
 
-            GridItem& tile = grid[i][j];
+            GridItem tile;
+
             tile.texture_id = texture_id;
             tile.x_start = (i - item_hover_i) * GRID_SIZE_PX;
             tile.y_start = (j - item_hover_j) * GRID_SIZE_PX;
@@ -36,11 +42,57 @@ void Grid::setGridTexture(TextureMap& textureMap, int texture_id)
             tile.y_end = tile.y_start + std::min(GRID_SIZE_PX, spare_y);
             spare_y -= GRID_SIZE_PX;
 
+
+            auto it = txtMap.find({i, j});
+
+            if (it == txtMap.end()) {
+
+                std::array<std::optional<GridItem>, 2> layers{};
+
+                layers[txtInMap.priority] = std::move(tile);
+                txtMap[{i, j}] = std::move(layers);
+
+            } else {
+
+                it->second[txtInMap.priority] = std::move(tile);
+            }
+
+            
         }
         spare_x -= GRID_SIZE_PX;
 
     }
 }
+
+
+
+
+
+
+bool Grid::thereAreAssignedTextures(TextureMap& textureMap, int texture_id) {
+
+    TextureInMap& txtInMap = textureMap.getTexture(texture_id);
+    SDL2pp::Texture& txt = txtInMap.txt;
+
+    int rows = std::ceil((float)txt.GetHeight() / GRID_SIZE_PX);
+    int columns = std::ceil((float)txt.GetWidth() / GRID_SIZE_PX);
+
+    for (int i = item_hover_i; i < item_hover_i + rows; i++) {
+
+        for (int j = item_hover_j; j < item_hover_j + columns; j++) {
+
+            auto it = txtMap.find({i, j});
+            if (it == txtMap.end())
+                break;
+
+            if (it->second[txtInMap.priority].has_value())
+                return true;
+
+        }
+
+    }
+}
+
 
 
 void Grid::render(SDL2pp::Renderer &renderer, TextureMap& textureMap)
@@ -52,6 +104,21 @@ void Grid::render(SDL2pp::Renderer &renderer, TextureMap& textureMap)
 
             GridItem& tile = grid[i][j];
 
+            renderGrass(renderer, textureMap, i , j);
+
+            /*
+
+            SDL2pp::Rect srcRect = { tile.x_start, tile.y_start, tile.x_end - tile.x_start, tile.y_end - tile.y_start };
+            SDL2pp::Rect dstRect = camera.toScreen(
+                (i - MAX_SIZE / 2) * GRID_SIZE_PX, 
+                (j - MAX_SIZE / 2) * GRID_SIZE_PX,
+                GRID_SIZE_PX,
+                GRID_SIZE_PX
+            );
+            
+            renderer.Copy(textureMap.getTexture(tile.texture_id).txt, srcRect, dstRect);
+
+            */
 
             SDL2pp::Rect dstRect = camera.toScreen(
                 (i - MAX_SIZE / 2) * GRID_SIZE_PX, 
@@ -60,30 +127,57 @@ void Grid::render(SDL2pp::Renderer &renderer, TextureMap& textureMap)
                 GRID_SIZE_PX
             );
 
-            SDL2pp::Rect srcRect = { 0, 0, GRID_SIZE_PX, GRID_SIZE_PX };
-            renderer.Copy(textureMap.getTexture(GRASS_TEXTURE_ID), srcRect, dstRect);
 
+            auto it = txtMap.find({(i - MAX_SIZE / 2) * GRID_SIZE_PX, (j - MAX_SIZE / 2) * GRID_SIZE_PX});
 
-            srcRect = { tile.x_start, tile.y_start, tile.x_end - tile.x_start, tile.y_end - tile.y_start };
-            dstRect = camera.toScreen(
-                (i - MAX_SIZE / 2) * GRID_SIZE_PX, 
-                (j - MAX_SIZE / 2) * GRID_SIZE_PX,
-                GRID_SIZE_PX,
-                GRID_SIZE_PX
-            );
-            
-            renderer.Copy(textureMap.getTexture(tile.texture_id), srcRect, dstRect);
+            if (it != txtMap.end()) {
+
+                for (auto& item : it->second) {
+
+                    if (item.has_value()) {
+                        GridItem& value = item.value();
+                        SDL2pp::Rect srcRect = { value.x_start, value.y_start, value.x_end - value.x_start, value.y_end - value.y_start };
+                        renderer.Copy(textureMap.getTexture(value.texture_id).txt, srcRect, dstRect);
+                    }
+                }
+            }
 
             if (hover_init && i == item_hover_i && j == item_hover_j) {
-                SDL_SetRenderDrawBlendMode(renderer.Get(), SDL_BLENDMODE_BLEND);
-                renderer.SetDrawColor(255, 0, 0, 100); 
-                renderer.FillRect(dstRect);
+                renderHover(renderer, dstRect);
             }
 
         }
 
     }
 
+}
+
+
+
+void Grid::renderHover(SDL2pp::Renderer& renderer, SDL2pp::Rect dstRect) {
+    SDL_SetRenderDrawBlendMode(renderer.Get(), SDL_BLENDMODE_BLEND);
+    renderer.SetDrawColor(255, 0, 0, 100); 
+    renderer.FillRect(dstRect);
+}
+
+
+
+void Grid::renderGrass(
+    SDL2pp::Renderer& renderer, 
+    TextureMap& textureMap, 
+    int i, int j
+) {
+
+
+    SDL2pp::Rect dstRect = camera.toScreen(
+        (i - MAX_SIZE / 2) * GRID_SIZE_PX, 
+        (j - MAX_SIZE / 2) * GRID_SIZE_PX,
+        GRID_SIZE_PX,
+        GRID_SIZE_PX
+    );
+
+    SDL2pp::Rect srcRect = { 0, 0, GRID_SIZE_PX, GRID_SIZE_PX };
+    renderer.Copy(textureMap.getTexture(GRASS_TEXTURE_ID).txt, srcRect, dstRect);
 }
 
 
@@ -100,3 +194,4 @@ void Grid::setMousePosition(int x, int y)
     item_hover_i = std::clamp(item_hover_i, 0, MAX_SIZE - 1);
     item_hover_j = std::clamp(item_hover_j, 0, MAX_SIZE - 1);
 }
+
