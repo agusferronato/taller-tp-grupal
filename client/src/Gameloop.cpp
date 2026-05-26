4bd323d4401b6aff952dab0073257176570e0970
Federico Tomas Parsons <143091832+xFedePumx@users.noreply.github.com>
2026-05-25 22:15:59 -0300

    Fix lo que dijo agus

--- client/src/Gameloop.cpp
+++ client/src/Gameloop.cpp
@@ -25,8 +25,8 @@ void Gameloop::run() {
   while (!shutdownEvent.finished()) {
 
     try {
-      gameModel->updateStateFromServer();
       gameController->update();
+      gameModel->updateStateFromServer();
       gameView->show(it);
 
     } catch (const ClosedQueue &e) {
