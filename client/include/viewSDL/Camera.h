65fdff6956345902518c68c9d5c7fc9b6c1d1777
tyreller <thiagoyair1@gmail.com>
2026-06-02 20:39:52 -0300

    merge dev into feature/chat

--- client/include/viewSDL/Camera.h
+++ client/include/viewSDL/Camera.h
@@ -4,17 +4,37 @@
 #include <SDL2pp/SDL2pp.hh>
 
 class Camera {
-
 private:
   float x{0.0f}, y{0.0f};
-  int screenW, screenH;
+
+  int viewportX{0};
+  int viewportY{0};
+  int viewportW;
+  int viewportH;
 
 public:
-  Camera(float screenW, float screenH);
+  Camera(int screenW, int screenH)
+      : viewportW(screenW), viewportH(screenH) {}
 
-  void follow(float targetX, float targetY, float targetW, float targetH);
+  void setViewport(int x, int y, int w, int h) {
+    viewportX = x;
+    viewportY = y;
+    viewportW = w;
+    viewportH = h;
+  }
 
-  SDL2pp::Rect toScreen(float wx, float wy, int w, int h) const;
+  void follow(float targetX, float targetY, float targetW, float targetH) {
+    x = targetX + targetW / 2.0f - viewportW / 2.0f;
+    y = targetY + targetH / 2.0f - viewportH / 2.0f;
+  }
+
+  SDL2pp::Rect toScreen(float wx, float wy, int w, int h) const {
+    return SDL2pp::Rect(
+        viewportX + static_cast<int>(wx - x),
+        viewportY + static_cast<int>(wy - y),
+        w,
+        h);
+  }
 };
 
 #endif
