e893f85eef517ecf17400be6237c813a43b966e0
tyreller <thiagoyair1@gmail.com>
2026-06-05 16:55:04 -0300

    fix: Players no longer keep connected even after.. disconnecting This happened if they didnt moved or interacted, because the lastPlayerId wasnt updated I removed this variable, as it was no longer needed.

--- server/src/Receiver.cpp
+++ server/src/Receiver.cpp
@@ -15,31 +15,16 @@ Receiver::Receiver(Socket &peer, Queue<ClientMessage> &gameloopQueue,
 void Receiver::kill() { keepRunning = false; }
 
 void Receiver::run() {
-
   while (keepRunning) {
-
     try {
       auto command = protocol.receiveCommand();
-
-      auto *move = std::get_if<MoveCommandDTO>(&command);
-      if (move) {
-        lastPlayerId = move->playerId;
-      }
-      auto *stop = std::get_if<PlayerStopCommandDTO>(&command);
-      if (stop) {
-        lastPlayerId = stop->playerId;
-      }
-      auto *exit = std::get_if<ExitCommandDTO>(&command);
-      if (exit) {
-        lastPlayerId = exit->playerId;
-      }
 
       gameloopQueue.push(ClientMessage{std::move(command), connectionId});
 
     } catch (const CommunicationEnded &e) {
-      if (keepRunning && lastPlayerId > 0) {
+      if (keepRunning) {
         gameloopQueue.push(
-            ClientMessage{ExitCommandDTO{lastPlayerId}, lastPlayerId});
+            ClientMessage{ExitCommandDTO{}, connectionId});
       }
       break;
 
