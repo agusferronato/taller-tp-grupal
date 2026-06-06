eddf4443cab4179cd1ab1060bdd5331ed44cde5d
Federico Tomas <fparsons@fi.uba.ar>
2026-06-06 18:08:07 -0300

    no se puede atacar en ciudad

--- server/include/City.h
+++ server/include/City.h
@@ -3,26 +3,17 @@
 
 #include "Position.h"
 
-
 class City {
 
-
 private:
-
-    Delimiter init, end;
-
+  Delimiter init, end;
 
 public:
-    City(Delimiter init, Delimiter end) : init(init), end(end) { };
+  City(Delimiter init, Delimiter end) : init(init), end(end) {}
 
-
-
+  bool contains(int x, int y) const {
+    return x >= init.column && x <= end.column && y >= init.row && y <= end.row;
+  }
 };
-
-
-
-
-
-
 
 #endif
