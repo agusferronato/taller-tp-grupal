fc49ff750802267e7f6888ba9c9e5178626a9ce3
Federico Tomas <fparsons@fi.uba.ar>
2026-06-07 14:23:58 -0300

    nombre NPC

--- server/include/NPC/Golem.h
+++ server/include/NPC/Golem.h
@@ -2,15 +2,21 @@
 #define GOLEM_H
 
 #include "NPC.h"
+#include "string"
 
 class Golem : public NPC {
 public:
-    Golem(Position position) : NPC(position) { }
-    NPCType getType() override { return NPCType::GolemT; }
+  explicit Golem(Position position) : NPC(position) {}
+  NPCType getType() override { return NPCType::GolemT; }
 
-    int getAncho() const override { return 32; };
-    int getAlto() const override { return 96; };
-    int getRange() const override { return 128; };
+  const std::string &getName() const override {
+    static const std::string name = "Golem";
+    return name;
+  }
+
+  int getAncho() const override { return 32; };
+  int getAlto() const override { return 96; };
+  int getRange() const override { return 128; };
 };
 
 #endif
--- server/include/NPC/Spider.h
+++ server/include/NPC/Spider.h
@@ -2,15 +2,21 @@
 #define SPIDER_H
 
 #include "NPC.h"
+#include "string"
 
 class Spider : public NPC {
 public:
-    Spider(Position position) : NPC(position) { }
-    NPCType getType() override { return NPCType::SpiderT; }
+  explicit Spider(Position position) : NPC(position) {}
+  NPCType getType() override { return NPCType::SpiderT; }
 
-    int getAncho() const override { return 48; };
-    int getAlto() const override { return 48; };
-    int getRange() const override { return 128; };
+  const std::string &getName() const override {
+    static const std::string name = "Spider";
+    return name;
+  }
+
+  int getAncho() const override { return 48; };
+  int getAlto() const override { return 48; };
+  int getRange() const override { return 128; };
 };
 
 #endif
